df525cc5023e2aec6f8bcdd445605abc0cc6d384
Nathan <nathanjohnhaight@gmail.com>
2025-09-16 11:19:20 -0600

    Added header.css and new_activity.html

--- activityTracker/core/views.py
+++ activityTracker/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Activity, TimeLog
 
 # Create your views here.
 def index(req):
     return render(req, "core/index.html")
+
+def new_activity(req):
+    return render(req, "core/new_activity.html")
