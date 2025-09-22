6b2f980e2fbbc239e5569ac0bd1004d2109ef773
Nathan <nathanjohnhaight@gmail.com>
2025-09-22 08:27:31 -0600

    Total time spent is now implemented

--- activityTracker/core/models.py
+++ activityTracker/core/models.py
@@ -1,15 +1,23 @@
 from django.db import models
+from datetime import timedelta
 
 # Create your models here.
 class Activity(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.TextField()
     
-    def activityTime(self):
-        return "Infinity Hours?!?!?!?"
+    def activity_time(self):
+        delta = timedelta()
+        for timelog in self.timelog_set.all():
+            delta += (timelog.end_time - timelog.start_time)
+
+        return str(delta)
 
 class TimeLog(models.Model):
     id = models.BigAutoField(primary_key=True)
     start_time = models.DateField()
     end_time = models.DateField()
     activity = models.ForeignKey("Activity", on_delete=models.CASCADE, name="log")
+
+    def time_spent(self):
+        return self.end_time - self.start_time
--- activityTracker/core/views.py
+++ activityTracker/core/views.py
@@ -3,14 +3,18 @@ from django.http import HttpRequest, JsonResponse
 from .models import Activity, TimeLog
 from datetime import datetime
 
-# Create your views here.
 def index(req: HttpRequest):
     activities = Activity.objects.all()
     return render(req, "core/index.html", {"activities": activities})
 
+#Gets the activity object and all its associated timelog objects
 def activity(req: HttpRequest, id):
     activity = Activity.objects.get(id=id)
-    return render(req, "core/activity.html", {"activity": activity})
+    timelogs = activity.timelog_set.all()
+    return render(req, "core/activity.html", {
+        "activity": activity,
+        "timelogs": timelogs
+        })
 
 def new_activity(req: HttpRequest):
     return render(req, "core/new_activity.html")
@@ -19,6 +23,7 @@ def new_timelog(req: HttpRequest, id):
     activity = Activity.objects.get(id=id)
     return render(req, "core/new_timelog.html", {"activity": activity})
 
+#Creates the activity object and sends you back to the index
 def create_activity(req: HttpRequest):
     query = req.POST
     name = query.get("name", "New Activity")
@@ -28,13 +33,18 @@ def create_activity(req: HttpRequest):
     activity.save()
     return redirect("/")
 
+#Creates a TimeLog object
 def create_timelog(req: HttpRequest, id):
-    activity = Activity.objects.get(id=id)
+    activity_key = Activity.objects.get(id=id)
     query = req.POST
     start = datetime.strptime(query.get("start-time"), '%Y-%m-%dT%H:%M')
     end = datetime.strptime(query.get("end-time"), '%Y-%m-%dT%H:%M')
-    print(f"Start Time: {start}")
-    print(f"End Time: {end}")
+    timelog = TimeLog(
+        start_time = start,
+        end_time = end,
+        log = activity_key
+    )
+    timelog.save()
     return redirect(f"/activity/{id}")
 
 
